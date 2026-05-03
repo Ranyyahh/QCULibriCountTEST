63b740226092669bef25ddf83bb792949d35e989
CaptainSylv <gano.jervchristian.atienza@gmail.com>
2026-05-04 00:16:29 +0800

    converted from sql to pl/sql

--- get_real_time_logs.php
+++ get_real_time_logs.php
@@ -2,52 +2,76 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "QCULibriCount";
+$username = "your_oracle_user";
+$password = "your_oracle_password";
+$connection_string = "localhost/XEPDB1"; // palitan base sa Oracle service mo
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = oci_connect($username, $password, $connection_string);
 
-if ($conn->connect_error) {
-    die(json_encode(["error" => "Connection failed"]));
+if (!$conn) {
+    $e = oci_error();
+    echo json_encode([
+        "success" => false,
+        "error" => "Connection failed: " . $e['message']
+    ]);
+    exit;
 }
 
-// Get recent activity
-$sql = "SELECT 
-            al.time_in,
-            al.time_out,
-            CASE 
-                WHEN al.time_out IS NULL THEN '+1 Entry'
-                ELSE '-1 Entry'
-            END as type,
-            CASE 
-                WHEN al.time_out IS NULL THEN 'Time in'
-                ELSE 'Time out'
-            END as action,
+$sql = "
+    SELECT 
+        CASE 
+            WHEN al.time_out IS NULL THEN '+1 Entry'
+            ELSE '-1 Entry'
+        END AS type,
+        CASE 
+            WHEN al.time_out IS NULL THEN 'Time in'
+            ELSE 'Time out'
+        END AS action,
+        TO_CHAR(
             CASE 
                 WHEN al.time_out IS NULL THEN al.time_in
                 ELSE al.time_out
-            END as event_time
-        FROM attendance_logs al
-        ORDER BY al.time_in DESC";
-        
-$result = $conn->query($sql);
-$activities = [];
+            END, 'HH:MI AM'
+        ) AS time,
+        TO_CHAR(
+            CASE 
+                WHEN al.time_out IS NULL THEN al.time_in
+                ELSE al.time_out
+            END, 'MM/DD/YY'
+        ) AS date_value
+    FROM attendance_logs al
+    ORDER BY al.time_in DESC
+";
 
-while($row = $result->fetch_assoc()) {
+$stid = oci_parse($conn, $sql);
+$r = oci_execute($stid);
+
+if (!$r) {
+    $e = oci_error($stid);
+    echo json_encode([
+        "success" => false,
+        "error" => $e['message']
+    ]);
+    oci_free_statement($stid);
+    oci_close($conn);
+    exit;
+}
+
+$activities = [];
+while ($row = oci_fetch_assoc($stid)) {
     $activities[] = [
-        'type' => $row['type'],
-        'action' => $row['action'],
-        'time' => date('h:i A', strtotime($row['event_time'])),
-        'date' => date('m/d/y', strtotime($row['event_time']))
+        'type' => $row['TYPE'],
+        'action' => $row['ACTION'],
+        'time' => $row['TIME'],
+        'date' => $row['DATE_VALUE']
     ];
 }
 
 echo json_encode([
-    'success' => true,
-    'activity' => $activities
+    "success" => true,
+    "activity" => $activities
 ]);
 
-$conn->close();
+oci_free_statement($stid);
+oci_close($conn);
 ?>
